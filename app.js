599562125d1bac2e908a755be5818c0df0dedd68
Lidi Faria <falecom@lidifaria.com>
2022-11-16 11:32:57 -0300

    Aula 16.11: alterações CSS, index e carrinho

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // 1 - Importar o express
 const express = require('express');
+const path = require('path');
 
-// 2 - Criar o servidor ou aplicação -> basta executar a função express, a mesma retorna um servidor
+// 2 - Criar o servidor
 const servidor = express();
 
-// 3 - Definir rota neste servidor
-// Endereço, método (get post put patch delete), função callback (a ação que o servidor vai realizar quando a req chegar)
-servidor.get('/usuarios', (req, res)=>{
-    console.log("chegou uma requisição!");
-    // return res.send("Permaneça em linha...");
+// Define a pasta public como sendo a pasta arquivos estáticos
+servidor.use(express.static(path.join(__dirname, 'public')))
+
+// 3 - Definir de uma rota neste servidor
+// endereço, método, função callback (a ação que o servidor vai realizar quando req chegar)
+servidor.get('/', (req, res)=>{
     return res.sendFile(__dirname + "/views/index.html");
 });
+
+servidor.get('/carrinho',(req, res)=>{
+    return res.sendFile(__dirname + "/views/carrinho.html");
+})
+
 
 // 4 - Por o servidor no modo "aguardando requisição"
 servidor.listen(3000);
